0542a4dedbed7486ba80ad250163c4f598d3a64b
catseye <cpressey@gmail.com>
2011-11-08 10:24:28 -0600

    Implement command-selecting command-line options.

--- falderal.hs
+++ falderal.hs
@@ -18,7 +18,6 @@ import Test.Falderal.Reporter (report)
 --
 
 data Flag = ReportFormat String
-          | ShowVersion
           | HaskellCommand String
           | ShellCommand String
           | Verbosity String
@@ -31,6 +30,14 @@ determineReportFormat (_:rest) = determineReportFormat rest
 determineVerbosity [] = 0
 determineVerbosity (Verbosity v:_) = (read v) :: Int
 determineVerbosity (_:rest) = determineVerbosity rest
+
+determineHaskellCommand [] = "ghc -e testModule"
+determineHaskellCommand (HaskellCommand s:_) = s
+determineHaskellCommand (_:rest) = determineHaskellCommand rest
+
+determineShellCommand [] = "sh"
+determineShellCommand (ShellCommand s:_) = s
+determineShellCommand (_:rest) = determineShellCommand rest
 
 --
 -- Command-line entry point
@@ -45,33 +52,32 @@ main = do
 
 header = "Usage: falderal <command> [<option>...] <filename.falderal>...\n\
          \where <command> is one of:\n\
+         \    format <format-name>\n\
          \    test\n\
-         \    format <format-name>"
+         \    version"
 
 options :: [OptDescr Flag]
 options = [
     Option ['h'] ["haskell-command"] (ReqArg HaskellCommand "CMD") "command to run Haskell tests (default: 'ghc -e testModule')",
     Option ['r'] ["report-format"] (ReqArg ReportFormat "FORMAT") "success/failure report format (default: standard)",
     Option ['s'] ["shell-command"] (ReqArg ShellCommand "CMD") "command to run shell scripts (default: 'sh')",
-    Option ['v'] ["verbosity"] (ReqArg Verbosity "LEVEL") "verbosity level, higher is more verbose (default: 0)",
-    Option ['V'] ["version"] (NoArg ShowVersion) "show version and exit"
+    Option ['v'] ["verbosity"] (ReqArg Verbosity "LEVEL") "verbosity level, higher is more verbose (default: 0)"
   ]
 
 dispatch ("format":formatName:fileNames) _ = do
     (lines, blocks) <- loadFiles fileNames
     putStr $ format formatName lines blocks
 
-dispatch ("test":fileNames) flags =
-    let
-        reportFormat = determineReportFormat flags
-        verbosity = determineVerbosity flags
-    in do
-        (lines, blocks) <- loadFiles fileNames
-        haskellBlocks <- return $ extractBlocks (isHaskellTest) blocks
-        shellBlocks <- return $ extractBlocks (isShellTest) blocks
-        testHaskell haskellBlocks reportFormat verbosity
-        testShell shellBlocks reportFormat verbosity
-        exitWith ExitSuccess
+dispatch ("test":fileNames) flags = do
+    (lines, blocks) <- loadFiles fileNames
+    haskellBlocks <- return $ extractBlocks (isHaskellTest) blocks
+    shellBlocks <- return $ extractBlocks (isShellTest) blocks
+    testHaskell haskellBlocks flags
+    testShell shellBlocks flags
+    exitWith ExitSuccess
+
+dispatch ("version":_) _ = do
+    putStrLn "Test.Falderal version 0.4"
 
 dispatch _ _ = putStrLn header
 
@@ -81,24 +87,28 @@ dispatch _ _ = putStrLn header
 -- TODO: require only runhaskell.
 -- TODO: allow "runhaskell" to be overridden with a cmd line opt.
 --
-testHaskell blocks reportFormat verbosity =
-    runTests blocks reportFormat verbosity
-        ("GeneratedFalderalTests.hs", "haskell", "ghc -e testModule GeneratedFalderalTests.hs")
+testHaskell blocks flags =
+    runTests blocks flags
+        ("GeneratedFalderalTests.hs", "haskell", (determineHaskellCommand flags) ++ " GeneratedFalderalTests.hs")
 
-testShell blocks reportFormat verbosity =
-    runTests blocks reportFormat verbosity
-        ("GeneratedFalderalTests.sh", "shell", "sh GeneratedFalderalTests.sh")
+testShell blocks flags =
+    runTests blocks flags
+        ("GeneratedFalderalTests.sh", "shell", (determineShellCommand flags) ++ " GeneratedFalderalTests.sh")
 
-runTests [] _ _ _= do
+runTests [] _ _ = do
     return ExitSuccess
-runTests blocks reportFormat verbosity (filename, formatName, command) = do
-    outputFileHandle <- openFile filename WriteMode
-    text <- return $ format formatName [] blocks
-    hPutStr outputFileHandle text
-    hClose outputFileHandle
-    exitCode <- system command
-    system ("rm -f " ++ filename)
-    return exitCode
+runTests blocks flags (filename, formatName, command) =
+    let
+        reportFormat = determineReportFormat flags
+        verbosity = determineVerbosity flags
+    in do
+        outputFileHandle <- openFile filename WriteMode
+        text <- return $ format formatName [] blocks
+        hPutStr outputFileHandle text
+        hClose outputFileHandle
+        exitCode <- system command
+        system ("rm -f " ++ filename)
+        return exitCode
 
 --
 -- This will create separate tests for each functionality named
