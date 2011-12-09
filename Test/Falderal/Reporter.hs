a1a753df28923273d71faf61194f379d3a15aa14
catseye <cpressey@gmail.com>
2011-12-09 16:27:29 -0600

    Lame start at making Haskell tests generate the failure dump format.
    --HG--
    rename : Test/Falderal/Reporter/Standard.hs => Test/Falderal/Reporter/FailureDump.hs

--- Test/Falderal/Reporter.hs
+++ Test/Falderal/Reporter.hs
@@ -7,12 +7,14 @@ module Test.Falderal.Reporter (report) where
 import System
 
 import qualified Test.Falderal.Reporter.Standard as Standard
+import qualified Test.Falderal.Reporter.FailureDump as FailureDump
 
 --
 -- Map from names of reporting styles to reporting functions.
 --
 
 getReporter "standard" = Standard.report
+getReporter "failure-dump" = FailureDump.report
 
 report format testTuples failures =
     (getReporter format) testTuples failures
