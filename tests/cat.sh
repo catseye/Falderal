84fb0198629ba4bc15b25faca9997c58b7a3c90c
Chris Pressey <cpressey@catseye.tc>
2022-07-29 17:09:41 +0100

    Revert "Checkpoint updating tests."
    This reverts commit a5fce627c5c376f5e8787560b9c4baa23be21388.

--- tests/cat.sh
+++ tests/cat.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-if [ "x$1" = "x-f" ]; then
-  if [ "x$3" = "x-o" ]; then
-    cat <"$2" >"$4"
-  else
-    cat <"$2"
-  fi
-elif [ "x$1" = "x-o" ]; then
+if [ "$1" == "-f"]; then
+  cat <"$2"
+elif [ "$1" == "-o"]; then
   cat >"$2"
 else
   cat
--- tests/echo.sh
+++ tests/echo.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-if [ "x$1" = "x-n" ]; then
-  printf %s "$2"
-else
-  printf %s\\n "$1"
-fi
+printf %s\\n $*
